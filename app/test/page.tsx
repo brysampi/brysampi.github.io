6cefadc12f171d4b0ae225dcd7a85c672ce9ed6f
Bryan ( BELL ) <bryansampi08@gmail.com>
2024-01-09 18:25:55 +0800

    add: Add nesting in tailwind css

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const name = () => {
+    return (
+        <div className='p-3'>
+            <div>test 1</div>
+            <Link href="test/test2">asdasd</Link>
+        </div>
+    )
+}
+
+export default name
---
+++ app/test/test2/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function name() {
+    return (
+        <div>test 2</div>
+    )
+}
