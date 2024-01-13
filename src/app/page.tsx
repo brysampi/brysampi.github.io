dc0b91c6776a951d72dde6ae5cd2d59421e5bd7a
Bryan ( BELL ) <bryansampi08@gmail.com>
2024-01-14 01:06:29 +0800

    update: Basic Displays - For the next update: Mobile Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,151 @@ export default function Home() {
           {/* Info */}
           <section>
             <div className="highlight text-[40px] font-bold">John Bryan Sampiano</div>
-            <div className="highlight-sub">but you can call me <span className="highlight">BELL</span></div>
-            <div className="highlight mt-5 mb-5 text-xl">Web Developer</div>
-            <div className="highlight-sub">As a web developer, building digital experiences is very close to my heart.</div>
+            <div className="highlight-sub text-[20px]  font-medium">but you can call me <span className="highlight">BELL</span></div>
+            <div className="highlight mt-[20px] mb-[20px] text-[20px]  font-medium">Web Developer</div>
+            <div className="highlight-sub mb-4">As a web developer, building digital experiences is very close to my heart.</div>
           </section>
           {/* About */}
           <section>
             <p className="pb-1"><span>ABOUT</span></p>
-            <p className="pt-2 pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente excepturi, provident consectetur suscipit tenetur accusantium cumque animi aliquid, culpa non minima perferendis expedita nobis dicta. Aspernatur a eaque provident labore.</p>
-            <p className="pt-2 pb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, sunt, soluta possimus placeat consectetur non corporis totam iure aperiam exercitationem officia maxime adipisci? In, alias impedit? Totam nesciunt voluptatibus consequatur.</p>
-            <p className="pt-2 pb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolor aliquam, quasi nobis accusamus, molestias inventore veniam voluptatem quisquam explicabo consectetur! Saepe similique delectus nostrum. Asperiores aut hic nam recusandae!</p>
+            <p className="pt-2 pb-2">I'm a web developer who has a strong passion for creating digital experiences. I've had the privilege of constructing web projects for a School to make things simple for students, from enrollment to subject management.</p>
+            <p className="pt-2 pb-2">I began my career as a web developer when I recognised the magic of writing code that brings websites to life. The creative process of turning ideas into interactive, visually beautiful realities is what I find most intriguing about web development. Over the years, I've honed my skills in front-end and back-end technologies like HTML, CSS, JavaScript, and PHP.</p>
+            <p className="pt-2 pb-2">I appreciate you visiting my webpage. Please feel free to look through my portfolio and get in touch if you have any questions or would want to discuss a project or share your ideas. Together, let's make your web vision a reality.</p>
+          </section>
+          {/* Experience */}
+          <section>
+            <p>Experience</p>
+            {/* Experience Card */}
+            <a href="" className='p-[20px] rounded-md flex flex-nowrap align-baseline'>
+              <div className='highlight-sub min-w-[120px] max-w-[120px] pr-[20px] font-bold'>FEB 2023 - FEB 2024</div>
+              <div>
+                <p className='highlight font-bold mb-0'>Web Development Engineer</p>
+                <p className='highlight-sub mt-0'>St.Dominic College of Asia</p>
+                <div className='pl-4 pr-4'>
+                  <p>Used the Codeigniter 3 Framework to create a variety of websites for students and staff.</p>
+                  <p>All prior system modules (.NET) should be converted to a web-based system.</p>
+                  <p>I am one of the programmers that construct the DOSE (Dominican One Stop Enrollment) / Admission portal, which students utilize to enroll themselves.</p>
+                  <p>When there is a system upgrade, works with the team to develop presentations for my task.</p>
+                  <div className='div_tag'>
+                    <div>CodeIgniter</div>
+                    <div>PHP</div>
+                    <div>SCSS/CSS</div>
+                    <div>JavaScript</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+            </a>
+            {/* Repeat Experience Card */}
+            <a href="" className='p-[20px] rounded-md flex flex-nowrap align-baseline'>
+              <div className='highlight-sub min-w-[120px] max-w-[120px] pr-[20px] font-bold'>FEB 2021 - FEB 2023</div>
+              <div>
+                <p className='highlight font-bold mb-0'>Intern : Web Developer</p>
+                <p className='highlight-sub mt-0'>TSPG IT Solutions</p>
+                <div className='pl-4 pr-4'>
+                  <p>Developed different websites with software division team for clients using Symfony 3 framework.</p>
+                  <div className='div_tag'>
+                    <div>CodeIgniter</div>
+                    <div>PHP</div>
+                    <div>SCSS/CSS</div>
+                    <div>JavaScript</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </section>
+          <section>
+            <p>PROJECTS</p>
+            <div className='w-full columns-2'>
+              {/* Project Card */}
+              <div className='projects_card'>
+                <img src="projects/onestop.png" alt="" />
+                <div className="projects_desc">
+                  <div className="title">Admission Portal</div>
+                  <div className="title_sub">Onestop · DOSE · Co-op</div>
+                  <div>
+                    Collaborated with our three-person team to develop a one-stop enrollment solution that
+                    allows students to sign up independently.
+                  </div>
+                  <div className="div_tag">
+                    <div>PHP</div>
+                    <div>CSS</div>
+                    <div>JavaScript</div>
+                    <div>CodeIgniter</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+              {/* Repeat Project Card */}
+              <div className='projects_card'>
+                <img src="projects/webdose.png" alt="" />
+                <div className="projects_desc">
+                  <div className="title">WebDose</div>
+                  <div className="title_sub">Maintenance · Update · Co-op</div>
+                  <div>
+                    For convenient access from locations other than the premises, all modules from the local system have been converted to a web-based system.
+                  </div>
+                  <div className="div_tag">
+                    <div>PHP</div>
+                    <div>CSS</div>
+                    <div>JavaScript</div>
+                    <div>CodeIgniter</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+              <div className='projects_card'>
+                <img src="projects/application_form.png" alt="" />
+                <div className="projects_desc">
+                  <div className="title">Application Form</div>
+                  <div className="title_sub">DOSE · Co-op</div>
+                  <div>
+                    Various forms that applicants can use to submit their personal data for the admissions process.
+                  </div>
+                  <div className="div_tag">
+                    <div>PHP</div>
+                    <div>CSS</div>
+                    <div>JavaScript</div>
+                    <div>CodeIgniter</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+              <div className='projects_card'>
+                <img src="projects/portal.png" alt="" />
+                <div className="projects_desc">
+                  <div className="title">Student Portal</div>
+                  <div className="title_sub">Maintenance · Update</div>
+                  <div>
+                    I've been entrusted with maintaining this system and adding and updating various features so that students can view their academic progress.
+                  </div>
+                  <div className="div_tag">
+                    <div>PHP</div>
+                    <div>CSS</div>
+                    <div>JavaScript</div>
+                    <div>CodeIgniter</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+              <div className='projects_card'>
+                <img src="projects/manipis.png" alt="" />
+                <div className="projects_desc">
+                  <div className="title">Manipis Lying in Clinic</div>
+                  <div className="title_sub">Capstone Project</div>
+                  <div>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos facilis perspiciatis voluptate, odit, exercitationem earum porro velit accusantium nisi nostrum et dolorem possimus assumenda natus dignissimos maxime esse, quaerat explicabo.
+                  </div>
+                  <div className="div_tag">
+                    <div>PHP</div>
+                    <div>CSS</div>
+                    <div>JavaScript</div>
+                    <div>JQuery</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </div>
