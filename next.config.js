161b29a8a504d205efecfaf9b368e8b085db9ae5
Bryan ( BELL ) <bryansampi08@gmail.com>
2024-01-17 13:47:38 +0800

    update: Remove CHAT GPT answer

--- next.config.js
+++ next.config.js
@@ -3,22 +3,7 @@
 // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
 const nextConfig = {
     // trailingSlash: true,
-    // output: 'export',
-
-
-    // From CHAT GPT answer
-    // ... other configurations ...
-    // Add the following export configuration
-    exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-    ) {
-        return {
-            // Your path mapping goes here
-            '/': { page: '/' },
-            // Add other paths as needed
-        };
-    },
+    output: 'export',
 }
 
 module.exports = nextConfig
